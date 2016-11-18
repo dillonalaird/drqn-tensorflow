c52db7b9f67defa6c818291e811813fb92108a53
Dillon Laird <dillonalaird@gmail.com>
2016-11-17 17:28:54 -0800

    indentation change

--- agents/experience.py
+++ agents/experience.py
@@ -3,62 +3,67 @@
 import random
 import numpy as np
 
+
 class Experience(object):
-  def __init__(self, data_format, batch_size, history_length, memory_size, observation_dims):
-    self.data_format = data_format
-    self.batch_size = batch_size
-    self.history_length = history_length
-    self.memory_size = memory_size
+    def __init__(self, data_format, batch_size, history_length, memory_size, observation_dims):
+        self.data_format = data_format
+        self.batch_size = batch_size
+        self.history_length = history_length
+        self.memory_size = memory_size
 
-    self.actions = np.empty(self.memory_size, dtype=np.uint8)
-    self.rewards = np.empty(self.memory_size, dtype=np.int8)
-    self.observations = np.empty([self.memory_size] + observation_dims, dtype=np.uint8)
-    self.terminals = np.empty(self.memory_size, dtype=np.bool)
+        self.actions = np.empty(self.memory_size, dtype=np.uint8)
+        self.rewards = np.empty(self.memory_size, dtype=np.int8)
+        self.observations = np.empty([self.memory_size] + observation_dims, dtype=np.uint8)
+        self.terminals = np.empty(self.memory_size, dtype=np.bool)
 
-    # pre-allocate prestates and poststates for minibatch
-    self.prestates = np.empty([self.batch_size, self.history_length] + observation_dims, dtype = np.float16)
-    self.poststates = np.empty([self.batch_size, self.history_length] + observation_dims, dtype = np.float16)
+        # pre-allocate prestates and poststates for minibatch
+        self.prestates = np.empty([self.batch_size, self.history_length] + observation_dims, dtype=np.float16)
+        self.poststates = np.empty([self.batch_size, self.history_length] + observation_dims, dtype=np.float16)
 
-    self.count = 0
-    self.current = 0
+        self.count = 0
+        self.current = 0
 
-  def add(self, observation, reward, action, terminal):
-    self.actions[self.current] = action
-    self.rewards[self.current] = reward
-    self.observations[self.current, ...] = observation
-    self.terminals[self.current] = terminal
-    self.count = max(self.count, self.current + 1)
-    self.current = (self.current + 1) % self.memory_size
+    def add(self, observation, reward, action, terminal):
+        print "add: count={}, current={}".format(self.count, self.current)
+        self.actions[self.current] = action
+        self.rewards[self.current] = reward
+        self.observations[self.current, ...] = observation
+        self.terminals[self.current] = terminal
+        self.count = max(self.count, self.current + 1)
+        self.current = (self.current + 1) % self.memory_size
 
-  def sample(self):
-    indexes = []
-    while len(indexes) < self.batch_size:
-      while True:
-        index = random.randint(self.history_length, self.count - 1)
-        if index >= self.current and index - self.history_length < self.current:
-          continue
-        if self.terminals[(index - self.history_length):index].any():
-          continue
-        break
-      
-      self.prestates[len(indexes), ...] = self.retreive(index - 1)
-      self.poststates[len(indexes), ...] = self.retreive(index)
-      indexes.append(index)
+    def sample(self):
+        print "sample: history_length={}, count={}, current={}" \
+            .format(self.history_length, self.count, self.current)
+        assert self.count > self.history_length
+        indexes = []
+        while len(indexes) < self.batch_size:
+            while True:
+                index = random.randint(self.history_length, self.count - 1)
+                if index >= self.current and index - self.history_length < self.current:
+                    continue
+                if self.terminals[(index - self.history_length):index].any():
+                    continue
+                break
 
-    actions = self.actions[indexes]
-    rewards = self.rewards[indexes]
-    terminals = self.terminals[indexes]
+            self.prestates[len(indexes), ...] = self.retreive(index - 1)
+            self.poststates[len(indexes), ...] = self.retreive(index)
+            indexes.append(index)
 
-    if self.data_format == 'NHWC' and len(self.prestates.shape) == 4:
-      return np.transpose(self.prestates, (0, 2, 3, 1)), actions, \
-        rewards, np.transpose(self.poststates, (0, 2, 3, 1)), terminals
-    else:
-      return self.prestates, actions, rewards, self.poststates, terminals
+        actions = self.actions[indexes]
+        rewards = self.rewards[indexes]
+        terminals = self.terminals[indexes]
 
-  def retreive(self, index):
-    index = index % self.count
-    if index >= self.history_length - 1:
-      return self.observations[(index - (self.history_length - 1)):(index + 1), ...]
-    else:
-      indexes = [(index - i) % self.count for i in reversed(range(self.history_length))]
-      return self.observations[indexes, ...]
+        if self.data_format == 'NHWC' and len(self.prestates.shape) == 4:
+            return np.transpose(self.prestates, (0, 2, 3, 1)), actions, \
+                rewards, np.transpose(self.poststates, (0, 2, 3, 1)), terminals
+        else:
+            return self.prestates, actions, rewards, self.poststates, terminals
+
+    def retreive(self, index):
+        index = index % self.count
+        if index >= self.history_length - 1:
+            return self.observations[(index - (self.history_length - 1)):(index + 1), ...]
+        else:
+            indexes = [(index - i) % self.count for i in reversed(range(self.history_length))]
+            return self.observations[indexes, ...]
