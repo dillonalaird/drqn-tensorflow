fixed tails of graph for moving average

graph_data.py
@@ -28,22 +28,10 @@ def graph_data_mul(datas, n=20):
         x = [ii for ii in xrange(len(data))]
         y = [xi[1] for xi in data]
         window = np.ones((n,))/n
-        smooth = np.convolve(y, window, mode="same")
-        plt.plot(x, y, alpha=0.3, linewidth=2.0, color=plt.rcParams["axes.color_cycle"][i])
-        plt.plot(x, smooth, linewidth=2.0, color=plt.rcParams["axes.color_cycle"][i])
+        smooth = np.convolve(y, window, mode="valid")
+        diff = int((len(y) - len(smooth))/2)
+        plt.plot(x, y, alpha=0.4, linewidth=2.0, color=plt.rcParams["axes.color_cycle"][i])
+        smooth_x = [ii for ii in xrange(diff, len(smooth) + diff)]
+        plt.plot(smooth_x, smooth, linewidth=2.0, color=plt.rcParams["axes.color_cycle"][i])
 
-    plt.show()
-
-
-def graph_data(data):
-    #plt.plot([x[0] for x in data], [x[1] for x in data])
-    x = [i for i in xrange(len(data))]
-    y = [xi[1] for xi in data]
-    n = 20
-    #window = signal.exponential(n, tau=25.)
-    #window = window/np.sum(window)
-    window = np.ones((n,))/n
-    smooth = np.convolve(y, window, mode="same")
-    plt.plot(x, y, alpha=0.3, linewidth=2.0, color=plt.rcParams["axes.color_cycle"][0])
-    plt.plot(x, smooth, linewidth=2.0, color=plt.rcParams["axes.color_cycle"][0])
     plt.show()